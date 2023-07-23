a9845f10920dfd0f9c9778ef1650e4da2f2480dd
Desiree Williams-Forde <130295888+desireealexia@users.noreply.github.com>
2023-07-23 04:20:35 +0100

    Update and rename Task 1 - hello_world.py to Task 1.py

--- Task 1 - hello_world.py
+++
@@ -1,5 +0,0 @@
-name = input("What is your name?")
-print(name)
-age = input("What is your age?")
-print(age)
-print("Hello World!")
---
+++ Task 1.py
@@ -0,0 +1,19 @@
+Part 1:
+
+name = input("What is your name?")
+print(name)
+age = input("What is your age?")
+print(age)
+print("Hello World!")
+
+
+Part 2:
+
+print("To form your meal deal please select a main, drink and snack!")
+item1 = input("What would you like for your main? ")
+item2 = input("What would you like for your drink? ")
+item3 = input("What would you like for your snack? ")
+print("You have selected the following items to form your meal deal: ")
+print(item1)
+print(item2)
+print(item3)
