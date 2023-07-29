da694517789fcc0a0463be8a99719a56abcd30c0
Desiree Williams-Forde <130295888+desireealexia@users.noreply.github.com>
2023-07-29 20:42:44 +0100

    Add files via upload

---
+++ task.py
@@ -0,0 +1,15 @@
+# Part 1:
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+
+sentence = sentence.replace("!" , " ")
+print(sentence)
+
+sentence = sentence.upper()
+print(sentence)
+
+sentence = sentence[::-1]
+print(sentence)
+
+#Part 2: 
+hero = "$$$Superman$$$"
+print(hero.strip('$'))
